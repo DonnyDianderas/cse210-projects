4dc841f1610ad5f579faec5f958d6ef56336983e
DonnyDianderas <donnydianderas@gmail.com>
2025-01-15 00:26:59 -0500

    submission activity Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Industrial Engineer";
+        job1._company = "Quimpac";
+        job1._startYear = 2015;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Control Room Panelist";
+        job2._company = "Cooperativa Naranjillo";
+        job2._startYear = 2013;
+        job2._endYear = 2015;
+
+        //Console.WriteLine(job1._company);
+        //Console.WriteLine(job2._company);
+
+        //job1.DisplayJob();
+        //Job2.DisplayJob();
+
+        Resume resume = new Resume();
+        resume._name = "Donny Dianderas";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayJob();
     }
+    
 }
