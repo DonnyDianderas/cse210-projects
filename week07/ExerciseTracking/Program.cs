1f8a01d0b045928391d24e14d5fa89bcf9fa7606
DonnyDianderas <donnydianderas@gmail.com>
2025-02-17 20:55:51 -0500

    Exercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+         List<Activity> _activities = new List<Activity>
+        {
+            new Running(new DateTime(2025, 02, 17), 60, 10.0),
+            new Cycling(new DateTime(2025, 02, 17), 30, 15.0),
+            new Swimming(new DateTime(2025, 02, 17), 30, 20)
+        };
+
+        foreach (Activity activity in _activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
